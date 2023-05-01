4e194e3fb6d4e87f8a5d3e6b26020a568270d59e
Priyanka Balivada <balivadapriyanka2519@gmail.com>
2023-05-01 18:47:21 +0530

    Arduino Code Added

---
+++ Temperature_Arduino_Code/Temperature_Arduino_Code.ino
@@ -0,0 +1,55 @@
+#include <Adafruit_MLX90614.h>
+#include <ESP8266WiFi.h>
+#include <ThingSpeak.h>
+long myChannelNumber = 2108652;
+const char myWriteAPIKey[] = "7TL3I8UCM99A5RH3";
+WiFiClient client;
+int buzzer=D5;
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  pinMode (buzzer,OUTPUT);
+  WiFi.begin("Galaxy F23","zlqv89pr");
+  while(WiFi.status() !=WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+
+  ThingSpeak.begin(client);
+  
+  Serial.begin(115200);
+  while (!Serial);
+
+  if (!mlx.begin()) {
+    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    while (1);
+  };
+}
+
+void loop() {
+//  float ambienttemp=mlx.readAmbientTempF();
+  float objecttemp=mlx.readObjectTempF();
+//  ThingSpeak.writeField(myChannelNumber, 1,ambienttemp, myWriteAPIKey);
+  ThingSpeak.writeField(myChannelNumber, 2,objecttemp, myWriteAPIKey);
+  
+  if(objecttemp>100.4)
+  {
+    digitalWrite(buzzer,HIGH) ; 
+  }
+  else
+  {
+    digitalWrite(buzzer,LOW) ; 
+  }
+  
+//  Serial.print("Ambient temperature = ");
+//  Serial.print(ambienttemp);
+//  Serial.print("°F");      
+//  Serial.print("   ");
+  Serial.print("Object temperature = "); 
+  Serial.print(objecttemp); 
+  Serial.println("°F");
+
+  Serial.println("-----------------------------------------------------------------");
+}
